7cd31cf0f744243ed954e82e67d76d860e97bb63
Roberto Mejía <robenriquemejia@gmail.com>
2021-10-26 15:04:18 -0600

    1

--- app/src/main/java/com/example/pm01ej12/MainActivity.java
+++ app/src/main/java/com/example/pm01ej12/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.pm01ej12;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtNombre, txtApellido, txtEdad, txtCorreo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtNombre = findViewById(R.id.txtNombre);
+        txtApellido = findViewById(R.id.txtApellido);
+        txtEdad = findViewById(R.id.txtEdad);
+        txtCorreo = findViewById(R.id.txtCorreo);
+
+        Button btnEnviar = findViewById(R.id.btnEnviar);
+
+        btnEnviar.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), Informacion.class);
+        });
     }
 }
